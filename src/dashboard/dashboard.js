2ecb3dbecea05f4c3855cba4e80e97ba255cdf6b
bamjam101 <herejamil123@gmail.com>
2022-12-21 22:19:52 +0530

    Created api call for user profile and added styles for user menu.

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,3 +1,14 @@
+const onProfileButtonClicked = (event) => {
+    event.stopPropagation();
+}
+
+const loadUserProfile = (event) => {
+    const defaultImage = documnet.querySelector("#default-profile");
+    const profileBtn = documnet.querySelector("#profile-btn");
+    const displayUsername = document.querySelector("#username");
+    profileBtn.addEventListener("click", onProfileButtonClicked);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Dom loaded.")
+    loadUserProfile();
 })
